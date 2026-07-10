f0205aeb7b06e8a4622f9024d91811b15550315c
François Lesecq <francois@MacBook-Air-de-Francois.local>
2026-07-10 17:12:24 +0200

    Configure service worker registerSW with skipWaiting and automatic page reload on update

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,19 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import { registerSW } from 'virtual:pwa-register'
+
+// Enregistrement du service worker avec mise à jour forcée automatique
+const updateSW = registerSW({
+  onNeedRefresh() {
+    console.log('[PWA] Nouvelle version disponible, application de la mise à jour...');
+    updateSW(true); // skipWaiting + window.location.reload() automatique
+  },
+  onOfflineReady() {
+    console.log('[PWA] Application prête pour le mode hors ligne.');
+  }
+});
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
